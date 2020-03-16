a6dd5f21eadfc75fc82154233da9cc07a5e8776b
VlKot <kotelchuk.v@gmail.com>
2020-03-16 20:01:12 +0200

    Updates

---
+++ precache-manifest.36f079fc6c414b70a496816a4e9fd8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf28b8a85dd9a08f22718c179a7230e",
+    "url": "/VIN-decoder/index.html"
+  },
+  {
+    "revision": "999f319a84d832e06fb2",
+    "url": "/VIN-decoder/static/css/main.4a4e4ab6.chunk.css"
+  },
+  {
+    "revision": "44aedae1be6bc4bc9bb4",
+    "url": "/VIN-decoder/static/js/2.bbecec1e.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/VIN-decoder/static/js/2.bbecec1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999f319a84d832e06fb2",
+    "url": "/VIN-decoder/static/js/main.5553cfe3.chunk.js"
+  },
+  {
+    "revision": "f958714ac19dc23ad472",
+    "url": "/VIN-decoder/static/js/runtime-main.1e61065a.js"
+  },
+  {
+    "revision": "68d0718fa16d082ca7e3f2ebcc48f956",
+    "url": "/VIN-decoder/static/media/preloader.68d0718f.svg"
+  }
+]);
